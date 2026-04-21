859c6fdd5b9da0cfb6413bf84556beb57923b2c3
SheaPettit <spettit2@horizon.csueastbay.edu>
2026-04-20 22:18:07 -0700

    updated test package and finished text message test

---
+++ test/chat/ChatListTest.java
@@ -0,0 +1,14 @@
+package test.chat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChatListTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
