0eeb73644fff88c2fb477c67733f5ee854ea26db
Denilson15 <galvarado1501@gmail.com>
2026-04-26 12:47:20 -0700

    Client has been refactored

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -9,70 +9,106 @@ import chat.TextMessage;
 import user.User;
 
 public class Client {
+	static InputStream serverInputStream = null;
+	static ObjectInputStream objectInputStream  = null;
+	static OutputStream outputStream = null;
+	static ObjectOutputStream objectOutputStream = null;
+	static List<Message> messageHistory = new ArrayList<>();
+	static Scanner sin = new Scanner(System.in);
+	
+	
+	public static boolean login(User user, ObjectOutputStream objectOutputStream, Socket clientSideSocket) throws IOException, ClassNotFoundException {
+        System.out.println(user.getUsername() + "attempting to log in...");
+        
+        Message loginRequestMessage = new Message(MainType.AUTHENTICATION, SubType.LOGIN, Status.REQUEST, user.getUsername() + "requesting login", user); //login message created
+        messageHistory.add(loginRequestMessage); //add the login message to the message history
+        
+        objectOutputStream.writeObject(loginRequestMessage); //sending the login message to server
+        
+        serverInputStream = clientSideSocket.getInputStream(); //whatever is coming in from the server
+        objectInputStream = new ObjectInputStream(serverInputStream); // we need to reconstruct the message object
+        
+        Message incomingLoginResponse = (Message) objectInputStream.readObject(); //deSerialized the message
+        messageHistory.add(incomingLoginResponse);
+        
+        if(incomingLoginResponse.status == Status.SUCCESS) {
+            System.out.println(incomingLoginResponse.getText() + "\n");
+            //List<textMessage> textMessageHistory = new ArrayList<>(); This isnt needed (likely will use this in communication system)
+            System.out.println("Enter text to send!\n");
+            return true;
+        }
+        else {
+        	System.out.println("Invalid Login. Please try again.");
+        	return false;
+        }
+	}
+	
+	public static boolean sendMessage(User user) throws IOException {
+		Message message;
+    	String text = "";
+    	boolean logOut = false;
+        while(!text.equals("!exitChat")) {
+        	text = sin.nextLine(); //read in user input
+            TextMessage textMessage = new TextMessage(text, user.getUsername(), user.getId()); //let 0 represent some userID
+
+            if(text.equals("!exitChat")){ //this sends a logout message to the server
+                message = new Message(MainType.AUTHENTICATION, SubType.LOGOUT, Status.REQUEST, textMessage.getText(), user);
+                messageHistory.add(message);
+                logOut = true;
+                objectOutputStream.writeObject(message); //where the object gets serialized and sent
+                return logOut;
+            }
+            else { //normal text being sent
+                 //user input is used to make textMessage object
+                message = new Message(MainType.TEXT, SubType.SEND_TEXT_MESSAGE , Status.REQUEST, textMessage.getText(), user);
+                messageHistory.add(message); //the message the user input should be sent
+                // textMessageHistory.add(textMessage); add the text message to the text message history array | This isnt needed (likely will use this in communication system)
+                objectOutputStream.writeObject(message); //where the object gets serialized and sent
+            }
+        }
+        return logOut;
+	}
+	
+	public static void listenForServerMessages(ObjectInputStream objectInputStream, Socket clientSideSocket) throws ClassNotFoundException, IOException {
+        List<Message> incomingServerMessages = (List<Message>) objectInputStream.readObject();
+        incomingServerMessages.forEach(msg -> {
+            if(msg.subType == SubType.LOGOUT) {
+                System.out.println("Logging out!"); //after user logs out we can close the client side socket
+                try {
+					clientSideSocket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+            else System.out.println("\nServer side echo " + clientSideSocket.getInetAddress().getHostAddress() + ": " + msg.getText() + '\n'); //display message along with who its from
+        });
+	}
+	
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Scanner sin = new Scanner(System.in); //cin == sin
+         //cin == sin
         int port = 7777;
         String host = "localhost";
 
         Socket clientSideSocket = new Socket(host, port); //create a client side socket that connects to server with the host and port specified
         System.out.println("Connected to: " + clientSideSocket.getInetAddress().getHostAddress());
 
-        OutputStream outputStream = clientSideSocket.getOutputStream(); //output that were sending to server
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream); //deconstructing the object were sending, this serializes the object
+        outputStream = clientSideSocket.getOutputStream(); //output that were sending to server
+        objectOutputStream = new ObjectOutputStream(outputStream); //deconstructing the object were sending, this serializes the object
 
         System.out.println("Enter Login!");
-        
-        String username = "Gary";// sin.nextLine() user would enter login info here
+		String username = "username";// sin.nextLine() user would enter login info here
         String password = "password";
         User user = new User(username, password);
-
-        Message loginRequestMessage = new Message(MainType.AUTHENTICATION, SubType.LOGIN, Status.REQUEST, user.getUsername() + "requesting login"); //login message created
-
-        List<Message> messageHistory = new ArrayList<>();
-        messageHistory.add(loginRequestMessage); //add the login message to the message history
-
-        objectOutputStream.writeObject(loginRequestMessage); //sending the login message to server
-
-        InputStream serverInputStream = clientSideSocket.getInputStream(); //whatever is coming in from the server
-        ObjectInputStream objectInputStream = new ObjectInputStream(serverInputStream); // we need to reconstruct the message object
-        Message incomingLoginResponse = (Message) objectInputStream.readObject(); //deSerialized the message
-
-        Message message;
         
-        if(incomingLoginResponse.status == Status.SUCCESS) {
-            System.out.println(incomingLoginResponse.getText() + "\n");
-            String text = "";
-            //List<textMessage> textMessageHistory = new ArrayList<>(); This isnt needed (likely will use this in communication system)
-            System.out.println("Enter text to send!\n");
-            while(!text.equals("!exitChat")) {
+        boolean authenticatedUser = login(user, objectOutputStream, clientSideSocket);
 
-                text = sin.nextLine(); //read in user input
-                TextMessage textMessage = new TextMessage(text, user.getUsername(), 0); //let 0 represent some userID
-
-                if(text.equals("!exitChat")){ //this sends a logout message to the server
-                    message = new Message(MainType.AUTHENTICATION, SubType.LOGOUT, Status.REQUEST, textMessage.getText());
-                    messageHistory.add(message);
-                }
-                else { //normal text being sent
-                     //user input is used to make textMessage object
-                    message = new Message(MainType.TEXT, SubType.SEND_TEXT_MESSAGE , Status.REQUEST, textMessage.getText());
-                    messageHistory.add(message); //the message the user input should be sent
-                    // textMessageHistory.add(textMessage); add the text message to the text message history array | This isnt needed (likely will use this in communication system)
-                }
-
-                objectOutputStream.writeObject(message); //where the object gets serialized and sent
-
-                Message incomingServerMessages = (Message) objectInputStream.readObject();
-                System.out.println("\nServer side echo " + clientSideSocket.getInetAddress().getHostAddress() + ": " + incomingServerMessages.getText() + '\n'); //display message along with who its from
-                messageHistory.add(incomingServerMessages);
-            }
-
-            message = (Message) objectInputStream.readObject();
-            if(message.subType == SubType.LOGOUT) {
-                System.out.println("Logging out!"); //after user logs out we can close the client side socket
-                clientSideSocket.close();
-            }
-            else System.out.println("We got a bug");
+        while(authenticatedUser) {
+        	if(!sendMessage(user))
+        		break;
+            listenForServerMessages(objectInputStream, clientSideSocket);
+//          messageHistory.add(incomingServerMessages);
         }
     }
+
 }
--- src/networking/Message.java
+++ src/networking/Message.java
@@ -3,6 +3,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Date;
 
+import user.User;
+
 public class Message implements Serializable { //serialize takes the object and converts into stream of bytes
     private static int count = 0;
     private int id;
@@ -12,6 +14,7 @@ public class Message implements Serializable { //serialize takes the object and 
     protected String text;
     protected Date date;
     protected Instant timeStamp;
+    protected User user;
 
     public Message(){
         id = count++;
@@ -21,9 +24,10 @@ public class Message implements Serializable { //serialize takes the object and 
         this.text = "Undefined";
         date = new Date();
         timeStamp = Instant.now();
+        user = null;
     }
 
-    public Message(MainType mainType, SubType subType, Status status, String text){
+    public Message(MainType mainType, SubType subType, Status status, String text, User user){
         id = count++;
         this.mainType = mainType;
         this.subType = subType;
@@ -31,6 +35,7 @@ public class Message implements Serializable { //serialize takes the object and 
         this.text = text;
         date = new Date();
         timeStamp = Instant.now();
+        this.user = user;
     }
 
     public MainType getType(){
